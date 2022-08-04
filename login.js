f6228cb2fa5d657c52334d3f775a77da303a140d
Mohamed mohamed ibrahim <110548351+M0hamed3@users.noreply.github.com>
2022-08-04 21:14:28 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,22 +1,11 @@
-function formValidatf() {
-    var firstName = document.getElementById("firstName").value;
-    var secondName = document.getElementById("secondName").value;
+function validate() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var error = document.getElementById("error");
     var text = "";
 
-    if (firstName < 4) {
-        text = "please Enter firstName more then 4";
-        error.innerHTML = text;
-        return false;
-    }
-    else if (secondName < 4) {
-        text = "please Enter secondName more then 4";
-        error.innerHTML = text;
-        return false;
-    }
-    else if (email.indexOf("@") == -1 || email.length < 12) {
+
+     if (email.indexOf("@") == -1 || email.length < 12) {
         text = "please Enter true email";
         error.innerHTML = text;
         return false;
